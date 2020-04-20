6ffe0a1349614e2a2626c8706cb50cbd0c2043ec
Jp3Stephens <JackStephens16@yahoo.com>
2020-04-19 17:45:31 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="outerwrap">
+        <div className="sideBar">
+          <div className="middleContainer">
+          middleContainer
+          </div>
+        side
+        </div>
+      outerwrap
+      </div>
+    </div>
+  );
+}
+
+export default App;
